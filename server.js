62dd001100ce85eb5229755f47372ce7a8343fcb
gurjarshraddhapatel <gurjarshraddha100@gmail.com>
2024-08-03 16:40:25 +0530

    update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set("view engine", "ejs");
 // Serve the public folder as static files
 app.use(express.static("public"));
 
-// Render the index template with default values for weather and error
+
 app.get("/",  (req, res) => {
   res.render("index", { weather: null, error: null });
 });
@@ -33,12 +33,10 @@ app.get("/weather", async (req, res) => {
     error = "Error, please try again";
 
   }
-  
-  // Render the index template with the weather data and error message
+
   res.render("index", { weather, error });
 });
 
-// Start the server and listen on port 3000 or the value of the PORT environment variable
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
